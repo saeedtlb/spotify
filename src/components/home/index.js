cbafc5f0dbad66f2be7c498d9b89a65bbf19a91f
saeed-tlb <saeedt7879s@gmail.com>
2020-09-17 17:20:46 +0430

    added router to the page for playlist page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,23 +2,29 @@ import React from 'react';
 
 import '../../Resources/Css/home.css';
 
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import LeftSideBar from './sidebar/LeftSideBar';
 import Player from './footer/Player';
-import Main from './main/Main';
+import Main from './main';
+import Playlist from './main/Playlist';
 
 const Body = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <div className='home'>
-                    <LeftSideBar />
-                    <Main />
-                    <Player />
-                </div>
-            </Switch>
-        </BrowserRouter>
+        <div className='home'>
+            <LeftSideBar />
+            <Player />
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={Main} />
+                    <Route
+                        exact
+                        path='/playlist/:play_id'
+                        component={Playlist}
+                    />
+                </Switch>
+            </BrowserRouter>
+        </div>
     );
 };
 
