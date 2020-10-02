37dffac731f655fc8b2a112773ef3c72dadc9ec3
saeed-tlb <saeedt7879s@gmail.com>
2020-10-02 16:55:25 +0330

    seperate routes and static components(leftsidebar player and qrcode comp which are always available)

---
+++ src/components/home/Routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+// ROUTES
+import Main from './main';
+import Playlist from './main/Playlist';
+import Categories from './main/category';
+import Category from './main/category/Category';
+
+import Freind from './sidebar/right/Freind';
+
+const Routes = () => {
+    return (
+        <div className='main'>
+            <Switch>
+                <Route exact path='/' component={Main} />
+                <Route exact path='/playlist/:play_id' component={Playlist} />
+                <Route exact path='/browse' component={Categories} />
+                <Route
+                    exact
+                    path='/category/:category_id'
+                    component={Category}
+                />
+            </Switch>
+            <Freind />
+        </div>
+    );
+};
+
+export default Routes;
