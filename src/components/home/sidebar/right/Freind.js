a7b416527a8099f6aba33d1e9175744361f4c425
saeed-tlb <saeedt7879s@gmail.com>
2020-10-02 16:58:25 +0330

    create right sidebar for freind activity

--- src/components/home/sidebar/right/Freind.js
+++ src/components/home/sidebar/right/Freind.js
@@ -13,7 +13,7 @@ import Drawer from '@material-ui/core/Drawer';
 const drawer_button = {
     open: {
         transform: 'rotate(180deg) translate(80%, -30%)',
-        backgroundColor: '#182131',
+        backgroundColor: '#fff',
         borderRadius: '0 50% 50% 0',
         cursor: 'pointer',
     },
@@ -31,99 +31,50 @@ const Freind = () => {
     const [{ display_name, image }] = useUserStateValue();
 
     const handleClick = () => {
-        // const main = document.querySelector('.main');
-        // if (!show) {
-        //     ref.current.style.gridColumn = '3/4';
-        //     main.style.gridColumn = '2/3';
-        // } else {
-        //     ref.current.style.gridColumn = 'auto';
-        //     main.style.gridColumn = '2/4';
-        // }
-
-        // console.log(ref.current.style.gridColumn);
+        ref.current.classList.toggle('close');
         setShow(prev => !prev);
     };
 
     return (
-        <Drawer
-            anchor='right'
-            open={show}
-            variant='persistent'
-            className='drawer'
-        >
-            {show ? (
-                <DoubleArrowRoundedIcon
-                    style={drawer_button.close}
-                    onClick={() => handleClick()}
-                />
-            ) : (
-                <DoubleArrowRoundedIcon
-                    style={drawer_button.open}
-                    onClick={() => handleClick()}
-                />
-            )}
-            <div className='user'>
-                {image ? (
-                    <Avatar src={image.url} alt={display_name} />
-                ) : display_name ? (
-                    <Avatar style={{ backgroundColor: '#46DB70' }}>
-                        {display_name.slice(0, 2).toUpperCase()}
-                    </Avatar>
-                ) : null}
-                <span>{display_name}</span>
+        <div className='freind' ref={ref}>
+            <div
+                className='right_side_icon'
+                data-status={show ? 'open' : 'close'}
+            >
+                {show ? (
+                    <DoubleArrowRoundedIcon onClick={() => handleClick()} />
+                ) : (
+                    <DoubleArrowRoundedIcon onClick={() => handleClick()} />
+                )}
             </div>
-
-            <div className='activity'>
-                <h3>freind activity</h3>
-                <div className='find_user'>
-                    <p>see what your freinds are playing</p>
-                    <button onClick={() => console.log('click')}>
-                        find freinds
-                    </button>
+            <Drawer
+                anchor='right'
+                open={show}
+                variant='persistent'
+                className='drawer'
+            >
+                <div className='user'>
+                    {image ? (
+                        <Avatar src={image.url} alt={display_name} />
+                    ) : display_name ? (
+                        <Avatar style={{ backgroundColor: '#46DB70' }}>
+                            {display_name.slice(0, 2).toUpperCase()}
+                        </Avatar>
+                    ) : null}
+                    <span>{display_name}</span>
                 </div>
-            </div>
-        </Drawer>
 
-        // <div className='freind' ref={ref}>
-        //     {show ? (
-        //         <DoubleArrowRoundedIcon
-        //             style={drawer_button.close}
-        //             onClick={() => handleClick()}
-        //         />
-        //     ) : (
-        //         <DoubleArrowRoundedIcon
-        //             style={drawer_button.open}
-        //             onClick={() => handleClick()}
-        //         />
-        //     )}
-        //     <Drawer
-        //         anchor='right'
-        //         open={show}
-        //         variant='persistent'
-        //         className='drawer'
-        //     >
-        //         <div className='user'>
-        //             {image ? (
-        //                 <Avatar src={image.url} alt={display_name} />
-        //             ) : display_name ? (
-        //                 <Avatar style={{ backgroundColor: '#46DB70' }}>
-        //                     {display_name.slice(0, 2).toUpperCase()}
-        //                 </Avatar>
-        //             ) : null}
-        //             <span>{display_name}</span>
-        //         </div>
-
-        //         <div className='activity'>
-        //             <h3>freind activity</h3>
-        //             <div className='find_user'>
-        //                 <p>see what your freinds are playing</p>
-        //                 <button onClick={() => console.log('click')}>
-        //                     find freinds
-        //                 </button>
-        //             </div>
-        //         </div>
-        //     </Drawer>
-        // </div>
+                <div className='activity'>
+                    <h3>freind activity</h3>
+                    <div className='find_user'>
+                        <p>see what your freinds are playing</p>
+                        <button onClick={() => console.log('click')}>
+                            find freinds
+                        </button>
+                    </div>
+                </div>
+            </Drawer>
+        </div>
     );
 };
 
