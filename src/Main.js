f09dd94b442607f5663cb854738450eefbf5740e
saeed-tlb <saeedt7879s@gmail.com>
2020-10-02 16:54:00 +0330

    change the file name

---
+++ src/Main.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+import { useUserStateValue } from './components/DataLayer';
+
+import Login from './components/Login';
+import Body from './components/home';
+
+const Main = () => {
+    const [{ token }] = useUserStateValue();
+
+    const remove_default_contextmenu = e => e.preventDefault();
+
+    useEffect(() => {
+        window.addEventListener('contextmenu', remove_default_contextmenu);
+
+        return () =>
+            window.removeEventListener(
+                'contextmenu',
+                remove_default_contextmenu
+            );
+    }, []);
+
+    return <>{token ? <Body /> : <Login />}</>;
+};
+
+export default Main;
