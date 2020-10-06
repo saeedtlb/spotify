a70d803504702c442d98344475883831b47a42fb
saeed-tlb <saeedt7879s@gmail.com>
2020-10-06 13:59:20 +0330

    add link for library options

--- src/components/utils/SidebarElement.js
+++ src/components/utils/SidebarElement.js
@@ -3,12 +3,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SidebarElement = ({ Icon = null, txt, style, id }) => {
+    const regex = /^(recently played|podcasts|local files)$/i;
     return (
         <div className='side_option' key={id}>
             {Icon ? (
                 <Link to={txt === 'home' ? '/' : `/${txt}`}>
                     <Icon />
                     <span style={style}>{txt}</span>
+                </Link>
+            ) : regex.test(txt) ? (
+                <Link to={`/${txt.replace(' ', '-')}`}>
+                    <p style={style}>{txt}</p>
                 </Link>
             ) : (
                 <Link to={`/playlist/${id}`}>
