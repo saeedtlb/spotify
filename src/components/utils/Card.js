24159f6ee10c93248fd8aecd64c61557fcb8ee9c
saeed-tlb <saeedt7879s@gmail.com>
2020-10-06 17:35:25 +0330

    add some more logic and jsx to be able to show the recently played song in a better form

--- src/components/utils/Card.js
+++ src/components/utils/Card.js
@@ -9,7 +9,8 @@ import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
-const Card = ({ url, name, description = null, linkTo, type }) => {
+const Card = ({ url, name, description = null, linkTo, type, ...rest }) => {
+    const regex = /^(playlist|song)$/i;
     return (
         <div className='card'>
             <Link to={linkTo} className='cover'>
@@ -18,7 +19,7 @@ const Card = ({ url, name, description = null, linkTo, type }) => {
                 ) : (
                     <img src='/Images/no_image.png' alt='playlist cover' />
                 )}
-                {type === 'playlist' ? (
+                {regex.test(type) ? (
                     <div className='mask'>
                         <FavoriteBorderIcon />
                         <PlayCircleOutlineIcon />
@@ -35,6 +36,11 @@ const Card = ({ url, name, description = null, linkTo, type }) => {
                     <p className='name'>{name}</p>
                     {description ? (
                         <p className='description'>{description}</p>
+                    ) : rest.song_type ? (
+                        <div className='type'>
+                            <p>{rest.artists}</p>
+                            <p>{rest.song_type}</p>
+                        </div>
                     ) : null}
                 </div>
             ) : null}
