d80a47bb7829a773a5a43cf5e0529a2a6a1fb4e3
saeed-tlb <saeedt7879s@gmail.com>
2020-10-06 17:34:39 +0330

    finish recently component which fetch data, store in store, show them and remove it when unmount

--- src/components/home/main/Recently.js
+++ src/components/home/main/Recently.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import '../../../Resources/Css/recently.css';
+
+import { recently_played } from '../../../Actions/song';
+import { useSongStateValue } from '../../DataLayer';
+
+import Card from '../../utils/Card';
+import Loading from '../../utils/Loading';
 
 const Recently = () => {
+    const [loading, setLoading] = useState([true, null]);
+    const [{ recently }, dispatch] = useSongStateValue();
+
+    useEffect(() => {
+        if (recently.length > 0) {
+            setLoading([false, null]);
+        } else {
+            recently_played()
+                .then(data => dispatch(data))
+                .catch(err => {
+                    console.log('Recently' + err);
+                    setLoading([false, 'somthing went wrong in getting data']);
+                });
+        }
+
+        return () =>
+            recently_played(true).catch(err =>
+                console.log('unmount recently' + err)
+            );
+    }, [recently.length]);
+
+    const renderCards = () =>
+        recently.map(song => (
+            <Card
+                key={song.id}
+                url={song.image}
+                name={song.name}
+                type='song'
+                song_type={song.type}
+                linkTo=''
+                artists={song.artists.join(', ')}
+            />
+        ));
+
     return (
-        <div>
-            <h1>recently played</h1>
-        </div>
+        <>
+            {loading[0] ? (
+                <Loading />
+            ) : loading[1] ? (
+                <div className='error'>
+                    <h1>{loading[1]}</h1>
+                </div>
+            ) : (
+                <div className='recently_played'>{renderCards()}</div>
+            )}
+        </>
     );
 };
 
