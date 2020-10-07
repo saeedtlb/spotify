0219cc8edd5831110b9c9deae5ba3060761b320a
saeed-tlb <saeedt7879s@gmail.com>
2020-10-07 13:20:54 +0330

    create a component for not found page which show a 404 illustration(svg)

---
+++ src/components/home/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ReactComponent as Logo } from './404.svg';
+
+const NotFound = () => {
+    return (
+        <div className='not_found'>
+            <Logo />
+        </div>
+    );
+};
+
+export default NotFound;
