eee5eb17d02600a74c83a456b08b53a95062b68c
saeed-tlb <saeedt7879s@gmail.com>
2020-10-07 13:21:30 +0330

    add a function to show the song date correctly

--- src/components/utils/misc.js
+++ src/components/utils/misc.js
@@ -1,2 +1,4 @@
 export const format_seconds = seconds =>
     Math.floor(seconds / 60) + ':' + ('0' + Math.floor(seconds % 60)).slice(-2);
+
+export const show_date = date => date.slice(0, date.indexOf('T'));
